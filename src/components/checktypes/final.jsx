5ddba6525a9795fe753d1cd4a79875d34918cb5c
jkingsman <jack.kingsman@gmail.com>
2019-02-16 16:46:02 -0800

    Add iamge saving system

--- src/components/checktypes/final.jsx
+++ src/components/checktypes/final.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import domtoimage from 'dom-to-image';
 
-import { CHECKTYPES } from "app/components/constants";
+import {
+    CHECKTYPES
+} from "app/components/constants";
 
 export default class Final extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imgBlob: null,
+        }
+        this.listRef = React.createRef();
+    }
+
+    componentDidMount() {
+        domtoimage.toPng(this.listRef.current)
+            .then(function(dataUrl) {
+                // var img = new Image();
+                // img.src = dataUrl;
+                // document.body.appendChild(img);
+                this.setState({imgBlob: dataUrl})
+            }.bind(this));
+    }
+
     render() {
         let checkSummary = [];
         let lastCategory;
-        this.props.checks.forEach(function(check){
-            if (check.checkType === CHECKTYPES.FINAL){
+        this.props.checks.forEach(function(check) {
+            if (check.checkType === CHECKTYPES.FINAL) {
                 return;
             }
 
-            if (check.category !== lastCategory){
+            if (check.category !== lastCategory) {
                 checkSummary.push(<span><strong>{check.category}</strong><br /></span>);
                 lastCategory = check.category;
             }
@@ -21,12 +42,15 @@ export default class Final extends React.Component {
         });
 
         return (
-            <div>
+            <div className="lastPage" ref={this.listRef}>
                 <h3 className="prompt">{this.props.item.prompt}</h3>
                 <hr />
                 <div className="finalContainer">
+                    Check complete at {new Date().toLocaleString()}
+                    <br />
                     {checkSummary}
                 </div>
+                {!this.state.imgBlob ? null : (<div><br /><br /><hr /><h3>Image Duplicate</h3><img src={this.state.imgBlob} /></div>)}
             </div>
         );
     }
